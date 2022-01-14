ab4bfc455704c653d244bea1045a8572032f214e
ssamin225 <sadmansamin225@gmail.com>
2022-01-14 18:37:44 -0500

    wip

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,43 @@
 // add middlewares here related to actions
+const Action = require('./actions-model');
+const Project = require('../projects/projects-model')
+
+const handleErrors = (err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message
+  });
+}
+
+const valiadateId = (req, res, next) => {
+  Action.get(req.params.id)
+    .then(action => {
+      if (action) {
+        req.action = action;
+        next();
+      } else {
+        next({ status: 404, message: 'not found!' });
+      }
+    }).catch(next)
+}
+
+const valiadateAction = (req, res, next) => {
+  const { project_id, description, notes } = req.body;
+  if (!project_id || !description || !notes) {
+    next({ status: 400, message: 'project id, description, and notes are required!' })
+  } else {
+    Project.get(req.body.project_id)
+    .then(project => {
+      if (project) {
+        next();
+      } else {
+        next({ status: 404, message: 'Project not found!' });
+      }
+    })
+  }
+}
+
+module.exports = {
+  handleErrors,
+  valiadateId,
+  valiadateAction,
+}
--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,39 @@
 // add middlewares here related to projects
+const Project = require('./projects-model');
+
+const valiadateId = (req, res, next) => {
+  Project.get(req.params.id)
+    .then(project => {
+      if (project) {
+        req.project = project;
+        next();
+      } else {
+        next({ status: 404, message: 'not found!' });
+      }
+    }).catch(next);
+}
+
+const valiadateProject = (req, res, next) => {
+  const { name, description, completed } = req.body;
+  if (!name || !description || completed === null) {
+    next({ status: 400, message: 'name, description, and completion status are required!' })
+  } else {
+    if (typeof completed === 'boolean') {
+      next();
+    } else {
+      next({ status: 400, message: 'completion status must be a boolean value!' })
+    }
+  }
+}
+
+const errorHandler = (err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message
+  });
+}
+
+module.exports = {
+  errorHandler,
+  valiadateId,
+  valiadateProject,
+};
