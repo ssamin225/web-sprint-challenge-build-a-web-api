f9fabb7c80171e9f160a138fa5c5fd1a2409a77f
ssamin225 <sadmansamin225@gmail.com>
2022-01-14 14:02:58 -0500

    wip

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,43 @@
 // Write your "actions" router here!
+const express = require('express');
+const Action = require('./actions-model');
+const { handleErrors, valiadateId, valiadateAction } = require('./actions-middlware')
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  Action.get()
+    .then(actions => {
+      res.status(200).json(actions);
+    }).catch(next);
+})
+
+router.get('/:id', valiadateId, (req, res, next) => {
+  res.status(200).json(req.action);
+})
+
+router.post('/', valiadateAction, (req, res, next) => {
+  Action.insert(req.body)
+    .then(newAction => {
+      res.status(201).json(newAction);
+    }).catch(next);
+})
+
+router.put('/:id', [valiadateId, valiadateAction], (req, res, next) => {
+  Action.update(req.params.id, req.body)
+    .then(updated => {
+      res.status(200).json(updated)
+    }).catch(next);
+})
+
+router.delete('/:id', valiadateId, (req, res, next) => {
+  const { id } = req.params;
+  Action.remove(id)
+    .then(deleted => {
+      res.status(200).json(`deleted action with the id ${id}`);
+    }).catch(next);
+})
+
+router.use(handleErrors);
+
+module.exports = router;
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,51 @@
+
 // Write your "projects" router here!
+const express = require('express');
+const Project = require('./projects-model');
+const { errorHandler, valiadateId, valiadateProject } = require('./projects-middleware')
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  Project.get()
+  .then(projects => {
+    res.status(200).json(projects);
+  }).catch(next);
+});
+
+router.get('/:id', valiadateId, (req, res, next) => {
+  res.status(200).json(req.project);
+})
+
+router.post('/', valiadateProject, (req, res, next) => {
+  Project.insert(req.body)
+    .then(newProject => {
+      res.status(201).json(newProject);
+    }).catch(next)
+})
+
+router.put('/:id', [valiadateId, valiadateProject], (req, res, next) => {
+  Project.update(req.params.id, req.body)
+    .then(updated => {
+      res.status(200).json(updated);
+    }).catch(next);
+})
+
+router.delete('/:id', valiadateId, (req, res, next) => {
+  const { id } = req.params;
+  Project.remove(id)
+    .then(deleted => {
+      res.status(200).json(`deleted project with the id ${id}`)
+    }).catch(next);
+})
+
+router.get('/:id/actions', valiadateId, (req, res, next) => {
+  Project.getProjectActions(req.params.id)
+    .then(actions => {
+      res.status(200).json(actions);
+    }).catch(next);
+})
+
+router.use(errorHandler);
+
+module.exports = router;
